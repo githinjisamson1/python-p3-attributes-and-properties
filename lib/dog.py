faa2e90c3cfc8ef3cf2004c9382a1190cea4ae58
githinjisamson1 <githinjisamson148@gmail.com>
2023-11-30 06:05:34 +0300

    Completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-APPROVED_BREEDS = [
+APPROVEDnameS = [
     "Mastiff",
     "Chihuahua",
     "Corgi",
@@ -11,5 +11,41 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
+
 class Dog:
-    pass
+
+    def __init__(self):
+        self._name = "Snoopy"
+        self._breed = "Mastiff"
+
+    def get_name(self):
+        print("Retrieving name...")
+        return self._name
+
+    def set_name(self, name):
+        if not isinstance(name, str) or not (1 <= len(name) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+
+        else:
+            print(f"Setting name to {name}")
+            self._name = name
+
+    def get_breed(self):
+        print("Retrieving breed...")
+        return self._breed
+
+    def set_breed(self, breed):
+        if breed not in APPROVEDnameS:
+            print("Breed must be in list of approved breeds.")
+        else:
+            return self._breed
+
+    name = property(get_name, set_name)
+
+    breed = property(get_breed, set_breed)
+
+
+fido = Dog()
+print(fido.name)
+print(fido.breed)
+# fido.name = "New name"
--- lib/person.py
+++ lib/person.py
@@ -15,5 +15,36 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
+
 class Person:
-    pass
+    def __init__(self):
+        self._name = "John"
+        self._job = None
+        
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if (isinstance(name, str)) and (len(name) <= 25):
+            self._name = name
+        else:
+            print("Name must be string under 25 characters.")
+
+    def get_job(self):
+        return self._job
+
+    def set_job(self, job):
+        if job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self._job = job
+            
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
+
+
+person1 = Person()
+print(person1.name)
+# person1.job="developer"
+# print(person1.job)
+# getattr(person1, "_job")
